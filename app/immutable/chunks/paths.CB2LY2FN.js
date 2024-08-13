712d50132dcc0ac71936706e459bdfd0794639c4
dlaiu <darryllaiu95@gmail.com>
2024-08-13 16:58:07 -0400

    Updates

--- app/immutable/chunks/paths.CB2LY2FN.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_9bz8c0)==null?void 0:s.base)??"/tiktok-accent";var t;const e=((t=globalThis.__sveltekit_9bz8c0)==null?void 0:t.assets)??a;export{e as a,a as b};
