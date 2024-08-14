fb6fee674b2aee5d1a9b76cc9e5b4d4461e5872e
dlaiu <darryllaiu95@gmail.com>
2024-08-14 09:49:58 -0400

    Updates

--- app/immutable/chunks/paths.CSSDaFt-.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_3c7dm8)==null?void 0:s.base)??"/tiktok-accent";var t;const e=((t=globalThis.__sveltekit_3c7dm8)==null?void 0:t.assets)??a;export{e as a,a as b};
--- app/immutable/entry/start.ZP1401ge.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.tmqBkB_b.js";export{t as start};
